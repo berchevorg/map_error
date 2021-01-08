8f42b6aad81f4677fe7c18ee3cc447a08bed8e36
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-01-08 10:07:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ variable "subnetworks" {
   type    = map
   default = {}
 }
+
+variable "region" {}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "110.0.0.0/16"
+}
+
 resource "null_resource" "cidrs" {
   for_each = var.subnetworks
   triggers = {
